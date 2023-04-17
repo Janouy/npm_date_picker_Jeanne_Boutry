cac13d4434fae986b73525d140165a1b4ef076d6
Jeanne Boutry <jeanne.boutry@hotmail.fr>
2023-04-17 11:16:12 +0200

    Add dist to gitignore

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,13 @@
 "use strict";
 
-var _react = _interopRequireDefault(require("react"));
-var _client = _interopRequireDefault(require("react-dom/client"));
-var _App = _interopRequireDefault(require("./App"));
-require("./index.css");
-var _context = require("./lib/utils/context");
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Calendar", {
+  enumerable: true,
+  get: function () {
+    return _Calendar.default;
+  }
+});
+var _Calendar = _interopRequireDefault(require("./components/Calendar"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-const root = _client.default.createRoot(document.getElementById("root"));
-root.render( /*#__PURE__*/_react.default.createElement(_react.default.StrictMode, null, /*#__PURE__*/_react.default.createElement(_context.DateProvider, null, /*#__PURE__*/_react.default.createElement(_App.default, null))));
